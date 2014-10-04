6dc33c444fd117502543339cc79f2cbd89c44ada
Ionut Bajescu <office@ionut-bajescu.ro>
2014-10-04 14:00:23 +0300

    add dumper

---
+++ src/Dumper.php
@@ -0,0 +1,111 @@
+<?php namespace Ionut\Dumper;
+
+/**
+ * Class Dumper
+ *
+ * @package Ionut\Dumper
+ */
+class Dumper {
+
+	/**
+	 * @var int
+	 */
+	protected $callNumber = 0;
+
+	/**
+	 * @var array
+	 */
+	protected $config;
+
+	/**
+	 * @var array
+	 */
+	protected $default_config = [
+		'clear'     => true,
+		'backtrace' => true,
+		'exit'      => true
+	];
+
+	/**
+	 * @param $config
+	 */
+	function __construct($config)
+	{
+		$this->config = $config + $this->default_config;
+	}
+
+
+	/**
+	 * @param $k
+	 *
+	 * @return array
+	 */
+	public function config($k)
+	{
+		return $this->config[$k];
+	}
+
+	/**
+	 * Dump parameters in pretty format.
+	 *
+	 * @param mixed $var,... unlimited Variables to be dumped.
+	 */
+	public function dump()
+	{
+		$this->callNumber++;
+
+
+		if ($this->config('clear')) {
+			$this->clearAllBuffers();
+		}
+
+		foreach (func_get_args() as $k => $var) {
+			echo '<pre>' . var_export($var, true) . '</pre>';
+		}
+
+		if ($this->config('backtrace')) {
+			echo $this->getBacktraceHtml();
+		}
+
+		if ($this->config('exit')) {
+			exit;
+		}
+	}
+
+	protected function clearAllBuffers()
+	{
+		while (@ob_end_clean()) {};
+	}
+
+	/**
+	 * Generate backtrace with clickable show-hide button.
+	 *
+	 * @return string
+	 */
+	protected function getBacktraceHtml()
+	{
+		$export = $this->getBacktrace();
+
+		return <<<HTML
+	<button
+		onclick="el=document.getElementById('backtrace{$this->callNumber}'); el.style.display = (el.style.display != 'none' ? 'none' : '' );"
+	>
+		Backtrace
+	</button>
+	<div id="backtrace{$this->callNumber}" style="display:none">
+		<pre>{$export}</pre>
+	</div>
+HTML;
+	}
+
+	/**
+	 * @return mixed
+	 */
+	protected function getBacktrace()
+	{
+		$error  = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS);
+		$export = var_export($error, true);
+
+		return $export;
+	}
+}
